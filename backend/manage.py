92dccc0935754f6607bde71d67bd34459c1d3ca0
Luu Nguyen <luunguyen@ccintegration.com>
2018-09-27 17:13:04 +0700

    Update

--- backend/manage.py
+++ backend/manage.py
@@ -4,6 +4,7 @@ import sys
 
 if __name__ == "__main__":
     try:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "common.settings")
         from django.core.management import execute_from_command_line
     except ImportError:
         # The above import may fail for some other reason. Ensure that the
